7c6e0623a1197c82b2783060a446d99979338606
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2021-10-26 22:43:23 +0200

    Add PresetDVD, for vardoto
    Added vardoto as a hard dep as I'm going to use it in the near future for a code refactor

--- parsedvd/IsoFile.py
+++ parsedvd/IsoFile.py
@@ -10,7 +10,10 @@ from abc import abstractmethod
 from pyparsedvd import vts_ifo
 from functools import lru_cache
 from itertools import accumulate
+from vardautomation.vpathlib import VPath
+from vardautomation.config import PresetType, Preset
 from typing import List, Union, Optional, Tuple, cast, Any
+
 
 from .DVDIndexers import DVDIndexer, D2VWitch
 from .dataclasses import IFOInfo
@@ -386,3 +389,12 @@ class __LinuxIsoFile(__IsoFile):
 
 
 IsoFile = __WinIsoFile if os_name == 'nt' else __LinuxIsoFile
+
+PresetDVD = Preset(
+    idx=lambda path: IsoFile(Path(path)).source(),
+    a_src=VPath('{work_filename:s}_track_{track_number:s}.wav'),
+    a_src_cut=VPath('{work_filename:s}_cut_track_{track_number:s}.wav'),
+    a_enc_cut=None,
+    chapter=None,
+    preset_type=PresetType.VIDEO
+)
--- parsedvd/__init__.py
+++ parsedvd/__init__.py
@@ -1,3 +1,3 @@
-from .IsoFile import IsoFile  # noqa
+from .IsoFile import IsoFile, PresetDVD  # noqa
 from .DVDIndexers import (DVDIndexer, D2VWitch, DGIndexNV, DGIndex)  # noqa
 from .dataclasses import (IFOInfo, IndexFileData, IndexFileVideo, IndexFileInfo)  # noqa
