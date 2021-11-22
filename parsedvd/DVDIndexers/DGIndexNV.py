a5d2479b09ff4c2a16a9052ff930b005c5528dbb
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2021-11-22 14:30:14 +0100

    Rewrite parsing logic for DGIndexNV

--- parsedvd/DVDIndexers/DGIndexNV.py
+++ parsedvd/DVDIndexers/DGIndexNV.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import vapoursynth as vs
-from functools import lru_cache
-from typing import Any, Callable, List, Union, Optional
+from functools import lru_cache, reduce as funcreduce
+from typing import Any, Callable, List, Union, Optional, Tuple
 
 from .DVDIndexer import DVDIndexer
 from ..dataclasses import IndexFileInfo, IndexFileData, IndexFileVideo, IndexFileVideoInfo
@@ -56,60 +56,39 @@ class DGIndexNV(DVDIndexer):
 
         file_content = '\n'.join(content)
 
-        with open(index_path, 'w') as file:
-            file.write(file_content)
+        # with open(index_path, 'w') as file:
+        #     file.write(file_content)
 
     @lru_cache
     def get_info(self, index_path: Path, file_idx: int = 0) -> IndexFileInfo:
-        f = index_path.open(mode="r", encoding="utf8")
+        with index_path.open(mode="r", encoding="utf8") as f:
+            file_content = f.read()
 
-        file_content = f.read()
+        lines = file_content.split('\n')
 
-        firstsplit_idx = file_content.index('\n\n')
+        head, lines = self.__split_lines(lines)
 
-        if "DGIndexNV" not in file_content[:firstsplit_idx]:
+        if "DGIndexNV" not in head[0]:
             self.file_corrupted(index_path)
 
-        cut_content = file_content[firstsplit_idx + 2:]
-
-        firstsplit = file_content[:firstsplit_idx].count('\n') + 2
-
-        maxsplits = cut_content[:cut_content.index('\n\n')].count('\n') + firstsplit + 1
-
-        f.close()
-
-        f = index_path.open(mode="r", encoding="utf8")
-
-        for _ in range(3):
-            f.readline().strip()
+        vid_lines, lines = self.__split_lines(lines)
 
         videos = [
             IndexFileVideo(Path(' '.join(line[:-1])), int(line[-1]))
-            for line in [
-                f.readline().strip().split(' ') for _ in range(maxsplits - firstsplit)
-            ]
+            for line in map(lambda a: a.split(' '), vid_lines)
         ]
 
-        if len(f.readline().strip()) > 0:
-            self.file_corrupted(index_path)
+        _, lines = self.__split_lines(lines)
 
-        while True:
-            if len(f.readline().strip()) == 0:
-                break
-
-        idx_file_sector = [0, 0]
-
-        for video in videos[:file_idx + 1]:
-            idx_file_sector[1] += video.size
+        idx_file_sector = [0, funcreduce(lambda a, b: a + b, [v.size for v in videos[:file_idx + 1]], 0)]
 
         idx_file_sector[0] = idx_file_sector[1] - videos[file_idx].size
 
         curr_SEQ = 0
 
         data = []
-        while True:
-            rawline = f.readline().strip()
 
+        for rawline in lines:
             if len(rawline) == 0:
                 break
 
@@ -135,35 +114,43 @@ class DGIndexNV(DVDIndexer):
                 continue
 
             matrix = getint(2)
+
             if matrix is not None:
                 matrix += 2
 
-            data.append(IndexFileData(
-                info=None, matrix=matrix,
-                vob=getint(4), cell=getint(5),
-                position=None, skip=0, pic_type=line[1]
-            ))
-
-        f.close()
+            data.append(
+                IndexFileData(
+                    info=None, matrix=matrix,
+                    vob=getint(4), cell=getint(5),
+                    position=None, skip=0, pic_type=line[1]
+                )
+            )
 
         vinfo_dict = {"film": 0.0, "frames_coded": 0, "frames_playback": 0, "order": 0}
 
-        def getfilmval(val: str) -> float:
-            return float(val.replace('%', ''))
-
-        for rlin in list(reversed(cut_content[-120:].split('\n')))[:8]:
+        for rlin in lines[-10:]:
             values = rlin.rstrip().split(' ')
 
             for key in vinfo_dict.keys():
-                if key.replace('frames_', '').upper() in values:
+                if key.split('_')[-1].upper() in values:
 
                     if key == 'film':
-                        value = getfilmval(values[0]) if '%' in values[0] else getfilmval(values[1])
+                        value = self.__getfilmval(values[0]) if '%' in values[0] else self.__getfilmval(values[1])
                     else:
                         value = int(values[1])
 
                     vinfo_dict[key] = value
 
-        video_info = IndexFileVideoInfo(**vinfo_dict)  # type: ignore
+        return IndexFileInfo(
+            index_path, videos, data, file_idx,
+            IndexFileVideoInfo(**vinfo_dict)  # type: ignore
+        )
 
-        return IndexFileInfo(index_path, videos, data, file_idx, video_info)
+    @staticmethod
+    def __split_lines(buff: List[str]) -> Tuple[List[str], List[str]]:
+        split_idx = buff.index('')
+        return buff[:split_idx], buff[split_idx + 1:]
+
+    @staticmethod
+    def __getfilmval(val: str) -> float:
+        return float(val.replace('%', ''))
