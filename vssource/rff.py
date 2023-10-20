174995a6a7178806279f3e408a07ca7096e705fb
jsaowji <39271213+jsaowji@users.noreply.github.com>
2023-10-20 20:05:40 +0200

    adjust rff logic

--- vssource/rff.py
+++ vssource/rff.py
@@ -1,6 +1,7 @@
 from vstools import vs, core, SPath, normalize_list_to_ranges, FrameRange
 from typing import List, Any
 from functools import partial
+from copy import deepcopy
 
 __all__ = [
     'apply_rff_array', 'apply_rff_video',
@@ -35,28 +36,21 @@ def apply_rff_array(old_array: List[any], rff: List[int], tff: List[int], prog: 
         f2 = array_double_rate[i * 2 + 1]
         if f1 != f2:
             print("Warning ambigious pattern due to rff {} {}".format(f1, f2))
+            print("This probably just means telecine across chapter boundary")
         array_return += [f1]
 
     return array_return
 
 
 def apply_rff_video(node: vs.VideoNode, rff: List[int], tff: List[int], prog: List[int], prog_seq: List[int]) -> vs.VideoNode:
-    assert len(node) == len(rff)
-    assert len(rff) == len(tff)
+    assert len(node) == len(rff) == len(tff) == len(prog) == len(prog_seq)
 
     fields = []
     tfffs = core.std.SeparateFields(core.std.RemoveFrameProps(node, props=["_FieldBased", "_Field"]), tff=True)
 
-    for i in range(len(rff)):
-        current_seq = prog_seq[i]
-        current_prg = prog[i]
-        current_rff = rff[i]
-
-        current_tff = tff[i]
-        current_bff = int(not current_tff)
-
-        if current_seq == 0:
-            if current_tff == 1:
+    for i, (current_prg_seq, current_prg, current_rff, current_tff) in enumerate(zip(prog_seq, prog, rff, tff)):
+        if not current_prg_seq:
+            if current_tff:
                 first_field = 2 * i
                 second_field = 2 * i + 1
             else:
@@ -64,11 +58,10 @@ def apply_rff_video(node: vs.VideoNode, rff: List[int], tff: List[int], prog: Li
                 second_field = 2 * i
 
             fields += [{"n": first_field, "tf": current_tff, "prg": False},
-                       {"n": second_field, "tf": current_bff, "prg": False}]
+                       {"n": second_field, "tf": not current_tff, "prg": False}]
             if current_rff:
                 assert current_prg
-                fields += [fields[-2]]
-
+                fields += [deepcopy(fields[-2])]
         else:
             assert current_prg
 
@@ -78,33 +71,35 @@ def apply_rff_video(node: vs.VideoNode, rff: List[int], tff: List[int], prog: Li
 
             fields += [{"n": 2 * i, "tf": 1, "prg": True}, {"n": 2 * i + 1, "tf": 0, "prg": True}] * cnt
 
-    # mark known progressive frames as progressive
-    for i in range(0, len(fields), 2):
-        tf = fields[i]
-        bf = fields[i + 1]
-        if tf["n"] % 2 == 0 and (bf["n"] - tf["n"] == 1):
-            current_prg = prog[tf["n"] // 2]
-            if current_prg:
-                tf["prg"] = True
-                bf["prg"] = True
+    # TODO: mark known progressive frames as progressive
 
     assert (len(fields) % 2) == 0
 
-    for a in range(len(fields) // 2):
-        tf = fields[a * 2]
-        bf = fields[a * 2 + 1]
-
-        # should this assert?
-        # assert tf["tf"] != bf["tf"]
+    for a, (tf, bf) in enumerate(zip(fields[::2], fields[1::2])):
         if tf["tf"] == bf["tf"]:
-            print("invalid field transition @{}".format(a))
+            bf["tf"] = not bf["tf"]
+            print(f"Invalid field transition @{a}")
+
+    for a in range(len(fields) // 2):
+        if fields[a * 2]["tf"] == fields[a * 2 + 1]["tf"]:
+            print("Could not fix sth for some reason", a)
+            assert False
 
     final = clip_remap_frames(tfffs, [x["n"] for x in fields])
 
-    final = core.std.RemoveFrameProps(final, props=["_FieldBased", "_Field"])
-    woven = core.std.DoubleWeave(final, tff=True)
+    def set_field(n, f, fields):
+        fout = f.copy()
+        fld = fields[n]
+        if "_FieldBased" in fout.props:
+            del fout["_FieldBased"]
+
+        fout.props['_Field'] = fld["tf"]
+        return fout
+
+    final = vs.core.std.ModifyFrame(clip=final, clips=final, selector=partial(set_field, fields=fields))
+
+    woven = core.std.DoubleWeave(final)
     woven = core.std.SelectEvery(woven, 2, 0)
-    woven = core.std.SetFieldBased(woven, 2)
 
     def update_progressive(n, f, fields):
         fout = f.copy()
