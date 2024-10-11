bb5c3918ca3efe8888daf57d5910c2b4093ca258
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-10-11 21:40:27 +0200

    `__init__`: top level noqa

--- vssource/formats/dvd/__init__.py
+++ vssource/formats/dvd/__init__.py
@@ -1,3 +1,5 @@
-from . import parsedvd  # noqa: F401,F403
-from .IsoFile import *  # noqa: F401,F403
-from .title import *  # noqa: F401,F403
+# ruff: noqa: F401, F403
+
+from . import parsedvd
+from .IsoFile import *
+from .title import *
